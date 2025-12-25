5ecd85d5549f09826da3cd15b2e21f9e41e7c21a
ranapratap <ranavishalsingh172770@gmail.com>
2025-12-25 16:58:22 +0530

    Adding Exceptions in UserServiceImpl.java

--- src/main/java/com/substring/auth/services/UserServiceImpl.java
+++ src/main/java/com/substring/auth/services/UserServiceImpl.java
@@ -1,10 +1,20 @@
 package com.substring.auth.services;
-
 import com.substring.auth.dtos.UserDto;
+import com.substring.auth.repo.UserRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements userService{
+
+    private final UserRepo userRepo;
+
     @Override
     public UserDto createUser(UserDto userDto) {
+        if (userDto.getEmail() == null || userDto.getEmail().isBlank()) {
+            throw new IllegalArgumentException("Email is required");
+        }
         return null;
     }
 
